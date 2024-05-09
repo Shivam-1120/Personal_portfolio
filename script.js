9f4c5f95da1c3c85dbcb0d99863f8c0bf013219d
Shivam Kumar <159603407+Shivam-1120@users.noreply.github.com>
2024-05-10 02:06:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 200) 
+});
+
+const sr = ScrollReveal ({
+    distance : '40px',
+    duration : 2050,
+    delay: 200,
+    reset: true
+});
+
+sr.reveal('.herotext',{delay:300, origin:'top'});
+sr.reveal('.aboutimg, .serviceitem, .abouttext',{delay:300, origin:'bottom'});
+sr.reveal('.abouttext h2, .textcenter, .rightcontact h2',{delay:300, origin:'top'});
+sr.reveal('.leftcontact',{delay:300, origin:'left'});
+sr.reveal('.rightcontact',{delay:300, origin:'right'});
+sr.reveal('.endsection',{delay:300, origin:'top'});
+sr.reveal('.projects',{delay:300, origin:'top'});
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
